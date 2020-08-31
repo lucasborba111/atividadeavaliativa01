37e972bbddbb5dceaad9eee642c3e75385078cab
lucasborba111 <lucasdbb.12@gmail.com>
2020-08-31 14:42:34 -0300

    feat exercicio29

---
+++ exercicio29.py
@@ -0,0 +1,7 @@
+numero1 = float(input("escreva um numero"))
+numero2 = float(input("escreva outro numero"))
+numero3 = float(input("escreva outro numero"))
+numero4 = float(input("escreva outro numero"))
+
+media = (numero1+numero2+numero3+numero4)/4
+print("media:",media)
